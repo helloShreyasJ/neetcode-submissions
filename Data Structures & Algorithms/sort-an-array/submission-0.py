cdf5d33685f09a2220351ab379cdd4db078f1f4d
Shreyas Joshi <76738983+helloShreyasJ@users.noreply.github.com>
2026-05-21 20:47:49 +0100

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        # bubble sort Big-O: O(n^2)    not ideal
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] > nums[j]:
+                    nums[i], nums[j] = nums[j], nums[i]
+
+        return nums
